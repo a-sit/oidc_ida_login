63cbbde22916ae5f56e4d0e48a7b0e83fea546f4
Thomas <>
2024-10-18 18:49:27 +0200

    fix: switch to new plugin name for link generation too

--- lib/OIDCLoginOption.php
+++ lib/OIDCLoginOption.php
@@ -39,7 +39,7 @@ class OIDCLoginOption implements IAlternativeLogin
 
     public static function getLoginLink(IRequest $request, IURLGenerator $url): string
     {
-        return $url->linkToRoute('oidc_login.login.oidc', [
+        return $url->linkToRoute('nextcloud-oidc-login.login.oidc', [
             'login_redirect_url' => $request->getParam('redirect_url'),
         ]);
     }
